5d189725c22aed8001fc586a35fb7e37d3001806
Mortaza Nalbandi <mortazana8@gmail.com>
2023-01-24 14:56:24 +0330

    the data base tables created and tested

--- php/database/create_database.php
+++ php/database/create_database.php
@@ -5,7 +5,7 @@ if (mysqli_connect_errno())
 die('Could not connect: ' . mysqli_connect_error()) ;
 }
 
-if (mysqli_query($con,"CREATE DATABASE phphw"))
+if (mysqli_query($con,"CREATE DATABASE finalProject"))
 {
 
 }
