eff39bc172ab6de945cc0f46a13b8d4cdd5ec679
Mortaza Nalbandi <mortazana8@gmail.com>
2023-01-24 20:28:01 +0330

    log in php finished and seccion creation done

---
+++ php/authentication/log_in.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    require('../database/connect_database.php');
+    if (isset($_SESSION["signedin"]) && $_SESSION["signedin"] == '1')
+        //header('Location: Dashboard.php?page=dash');
+        echo 'already loged';
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $acc_password = $_POST['password'];
+        $sql="SELECT * FROM Users where username = '" .$username . "'";
+        if (($result=mysqli_query($con,$sql))){
+            if(mysqli_num_rows($result)==1){
+                $row=mysqli_fetch_row($result);
+
+                if (!empty($username) && !empty($acc_password) && $username == $row[1] && $acc_password == $row[2]) {
+                    $_SESSION['signedin'] = 1;
+                    $_SESSION['username'] =  $row[0];
+                    //header('Location: Dashboard.php?page=dash');
+                    echo 'Loged In.'; 
+                }    
+            }else{
+                echo 'You must enter a correct username and password.'; 
+            }                             
+        }else
+        {
+        echo "Error Inserting table: " . mysqli_error($con);
+        }
+    }
+?>
