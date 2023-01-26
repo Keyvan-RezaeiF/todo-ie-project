d888fae5c37efb825e363a36d8cfc82a0b4fdfaf
Mortaza Nalbandi <mortazana8@gmail.com>
2023-01-26 10:35:26 +0330

    tabels refrmated

---
+++ php/add_Catagory.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    require('database/connect_database.php');
+    if (!isset($_SESSION["signedin"]) && $_SESSION["signedin"] != '1'){
+        header('Location: index.html');
+    }
+?>
+<html>
+
+<head>
+
+    <title>Log In</title>
+
+</head>
+
+<body>
+
+
+    <form action="database\insert_catagory.php" method="POST">
+        <br>Title: <input type="text" name="title">
+        <br><input type="submit" value="add Catagory">
+    </form>
+
+</body>
+</html>
