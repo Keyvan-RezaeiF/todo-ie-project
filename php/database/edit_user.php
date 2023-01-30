502d5617440d3ab5cd4e2b374bf8000f388e59fb
Mortaza Nalbandi <mortazana8@gmail.com>
2023-01-30 19:53:24 +0330

    ses=ssion bug done 2

--- php/database/edit_user.php
+++ php/database/edit_user.php
@@ -10,16 +10,19 @@
         $sql=" UPDATE Users
         SET username = '" .$username . "' , first_name = '" .$firstname . "',last_name = '" .$lastname . "'
         WHERE userID =  '" .$userID . "'; ";
+        
     } else {
         $sql=" UPDATE Users
         SET username = '" .$username . "' , acc_password = '" .$acc_password . "' , first_name = '" .$firstname . "',last_name = '" .$lastname . "'
         WHERE userID =  '" .$userID . "'; ";
     }
-
-        if (mysqli_query($con, $sql)) {
-            echo "1 Record updated";
-        } else {
-            echo "Error inserting User: " . mysqli_error($con);
-        }
+    $_SESSION['username'] =  $username;
+    $_SESSION['first_name'] =  $firstname;
+    $_SESSION['last_name'] =  $lastname;
+    if (mysqli_query($con, $sql)) {
+        echo "1 Record updated";        
+    } else {
+        echo "Error inserting User: " . mysqli_error($con);
+    }
     header('Location: ../../todos.php');
 ?>
