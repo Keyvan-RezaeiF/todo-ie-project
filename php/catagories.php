329795260aaa52634f9e3269148b3c3b2f47a62a
Mortaza Nalbandi <mortazana8@gmail.com>
2023-01-31 08:57:05 +0330

    todos link buf dix

--- php/catagories.php
+++ php/catagories.php
@@ -7,7 +7,7 @@
       <?php
         require('database/connect_database.php');
         if (!isset($_SESSION["signedin"]) && $_SESSION["signedin"] != '1'){
-          header('Location: ../php/authentication/log_in.html');
+          header('Location: ../todo-ie-project/php/authentication/log_in.html');
         }
         $userID = $_SESSION['userID'];
         $sql="SELECT * FROM Catagory where userID ='" .$userID . "'";
